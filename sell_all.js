e056f46dacaa3f10122fd6fb4058def93530f5c5
unknown <makeev.eugene@gmail.com>
2019-04-03 01:41:30 +0300

    fixed api keys import, minor adjust in output

--- sell_all.js
+++ sell_all.js
@@ -1,12 +1,13 @@
 /**
  * 
  */
-import "key.js"
-var bittrex = require('./node.bittrex.api/node.bittrex.api.js');
+
+const keys = require('./key.js');
+var bittrex = require('node-bittrex-api/node.bittrex.api.js');
 
 bittrex.options({
-  'apikey' : API_KEY,
-  'apisecret' : API_SECRET,
+  'apikey' : keys.API_KEY,
+  'apisecret' : keys.API_SECRET,
   'verbose' : true,
   'cleartext' : false,
   'baseUrl' : 'https://bittrex.com/api/v1.1',
@@ -25,9 +26,10 @@ bittrex.options({
     	    return console.error(err);
     	  }
 
-    	  available = data.result.filter(balance => balance.Available > 0);
-          console.log("\n\n\nCurrencies available:", available.length ,"\n", available);
+    	  available = data.result.filter(balance => balance.Balance > 0);
+          console.log("\n\n\nCurrencies available:", available.length);
           available.forEach(function(element) {
+			  console.log("\t", element.Currency, ":", element.Balance);
         	  if (element.Currency === 'BTC') {
         		  markets.push('USDT-BTC');
         	  }
@@ -35,8 +37,6 @@ bittrex.options({
         		  markets.push('BTC-' + element.Currency);
         	  }
      	  });
-          //markets = Array.from({length : available.length}, (e, i) => 'BTC-' + available[i].Currency);
-          console.log(markets);
           
           bittrex.websockets.subscribe(markets, function(data) {
             if (data.M === 'updateExchangeState') {
